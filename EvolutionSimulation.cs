d619076beca7c46308c38ff4b68e711539ee1ed2
Ali <ali.mehdi.jaffer@gmail.com>
2022-04-22 09:08:47 -0400

    Commit 2

--- EvolutionSimulation.cs
+++ EvolutionSimulation.cs
@@ -1,63 +1,39 @@
 ﻿using System;
 
 namespace Helloworld
-{    class Entity {
-        public int health = 3;
-        public int aggressiveness = 10;
-
-        public Entity(int aggressivenessInt) {
-            if (aggressivenessInt >= 100) {
-                aggressiveness = 100;
-            } else {
-                aggressiveness = aggressivenessInt;
-            }
+{  
+    static public class Prey {
+        static public float Rabbits = 10;
+        static public float Foxes = 0;
+    }  
+    class Hunter 
+    {
+        float speed = 4;
+        float sight = 4;
+        float stealth = 4;
+        Hunter(float speedFloat, float sightFloat, float stealthFloat) {
+            speed = speedFloat;
+            sight = sightFloat;
+            stealth = stealthFloat;
         }
-        public void Attack(Entity target) {
-            Random RNG = new Random();
-            int CoinFlip = RNG.Next(1, 3);
-            if (CoinFlip == 1) {
-                health -= 2;
-            } else {
-                target.health--;
-                health++;
-            }
+        void Hunt() {
+            Prey.Rabbits--;
         }
+    }
+    class Bunny {
+        float speed = 2;
+        float vigilance = 5;
+        float stealth = 3;
+    }
+    class Fox {
+        float speed = 6;
+        float vigilance = 2;
+        float stealth = 1;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Random Rand = new Random();
-            List<Entity> EntityList = new List<Entity>();
-            for (int day = 0; day < 5; day++) {
-                Console.WriteLine("Day " + day);
-                for (int i = 0; i < 5; i++) {
-                    EntityList.Add(new Entity(50));
-                }
-                Console.WriteLine("Entities: " + EntityList.Count);
-                foreach (Entity TargetEntity in EntityList) {
-                    if (Rand.Next(1, 101) <= TargetEntity.aggressiveness) {
-                        Entity AttackEntity = new Entity(10);
-                        while (true) {
-                            AttackEntity = EntityList[Rand.Next(0, EntityList.Count)];
-                            if (EntityList.IndexOf(AttackEntity) != EntityList.IndexOf(TargetEntity)) {
-                                Console.WriteLine("Entity" + EntityList.IndexOf(TargetEntity) + " attacked " + "Entity" + EntityList.IndexOf(AttackEntity));
-                                break;
-                            }
-                        }
-                        TargetEntity.Attack(AttackEntity);
-                    }
-                }
-                for (int x = 0; x < EntityList.Count; x++) {
-                    if (EntityList[x].health <= 0) {
-                        Console.WriteLine("Entity" + x + " Died");
-                        EntityList.Remove(EntityList[x]);
-                        x--;
-                    } else {
-                        Console.WriteLine("Health of Entity" + x + ": " + EntityList[x].health);
-                    }
-                }
-            }
         }
     }
 }
