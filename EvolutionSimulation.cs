e78bea9f39a6723fa558c9f06da7c95a256caf07
Ali <ali.mehdi.jaffer@gmail.com>
2022-04-22 12:57:21 -0400

    Commit 3

--- EvolutionSimulation.cs
+++ EvolutionSimulation.cs
@@ -1,39 +1,89 @@
 ﻿using System;
+using System.IO;
 
 namespace Helloworld
 {  
-    static public class Prey {
-        static public float Rabbits = 10;
-        static public float Foxes = 0;
+    static public class Entities {
+        static public int Rabbits = 10;
+        static public int Day = 1;
+
+        static public List<Hunter> HunterList = new List<Hunter>();
+        static public List<Hunter> NewHunterList = new List<Hunter>();
+
     }  
-    class Hunter 
+    public class Hunter 
     {
-        float speed = 4;
-        float sight = 4;
-        float stealth = 4;
-        Hunter(float speedFloat, float sightFloat, float stealthFloat) {
+        public float speed = 4;
+        public float sight = 4;
+        public float stealth = 4;
+        public float food = 0;
+
+        public string name = "0";
+        public Hunter(float speedFloat, float sightFloat, float stealthFloat, string nameString) {
             speed = speedFloat;
             sight = sightFloat;
             stealth = stealthFloat;
+            name = nameString;
         }
-        void Hunt() {
-            Prey.Rabbits--;
+        public void Hunt() {
+            Random RNG = new Random();
+            int Dice = RNG.Next(1, 7);
+            float Chance = 0;
+            if (speed > Bunny.speed) {
+                Chance += speed - Bunny.speed;
+            }
+            if (sight > Bunny.stealth) {
+                Chance += sight - Bunny.stealth;
+            }
+            if (stealth > Bunny.vigilance) {
+                Chance += stealth - Bunny.vigilance;
+            }
+            int Bunnies = Entities.Rabbits;
+            for (int i = 0; i < Bunnies; i++) {
+                Dice = RNG.Next(1, 7);
+                if (Chance > Dice) {
+                    Entities.Rabbits--;
+                    food++;
+                }
+            }
+        }
+        public void Reproduce() {
+            if (food >= 1) {
+                Entities.NewHunterList.Add(new Hunter(4, 4, 4, name));
+            }
+            if (food >= 2) {
+                for (int z = 1; z < food; z++) {
+                    Entities.NewHunterList.Add(new Hunter(4, 4, 4, name + Entities.Day));
+                }
+            }
         }
     }
-    class Bunny {
-        float speed = 2;
-        float vigilance = 5;
-        float stealth = 3;
-    }
-    class Fox {
-        float speed = 6;
-        float vigilance = 2;
-        float stealth = 1;
+    public static class Bunny {
+        static public float speed = 2;
+        static public float vigilance = 5;
+        static public float stealth = 3;
     }
     class Program
     {
         static void Main(string[] args)
         {
+            File.Delete("EvolutionSimulationOutput.txt");
+            using StreamWriter outputFile = new StreamWriter("EvolutionSimulationOutput.txt");
+
+            Entities.HunterList.Add(new Hunter(4, 4, 4, "A"));
+            for (int y = 1; y < 6; y++) {
+                Entities.Day = y;
+                outputFile.WriteLine(string.Concat(Enumerable.Repeat("-", 100)) + "\n" + "Day: " + Entities.Day + "\n" + string.Concat(Enumerable.Repeat("-", 100)));
+                Entities.Rabbits = 10;
+                Entities.NewHunterList.Clear();
+                foreach (Hunter Target in Entities.HunterList) {
+                    Target.Hunt();
+                    outputFile.WriteLine("Name: " + Target.name + "\nFood: " + Target.food + "\n");
+                    Target.Reproduce();
+                }
+                Entities.HunterList.Clear();
+                Entities.HunterList.AddRange(Entities.NewHunterList);
+            }
         }
     }
 }
