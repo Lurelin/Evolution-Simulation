045decc263820d52a1d2fc61f497a38341f26072
Ali <ali.mehdi.jaffer@gmail.com>
2022-04-22 13:20:39 -0400

    Commit 6

--- EvolutionSimulation.cs
+++ EvolutionSimulation.cs
@@ -7,6 +7,8 @@ namespace Helloworld
         static public int Rabbits = 10;
         static public int Day = 1;
 
+        static public int NamesUsed = 0;
+
         static public List<Hunter> HunterList = new List<Hunter>();
         static public List<Hunter> NewHunterList = new List<Hunter>();
 
@@ -18,12 +20,12 @@ namespace Helloworld
         public float stealth = 4;
         public float food = 0;
 
-        public string name = "0";
-        public Hunter(float speedFloat, float sightFloat, float stealthFloat, string nameString) {
+        public int name;
+        public Hunter(float speedFloat, float sightFloat, float stealthFloat, int nameInt) {
             speed = speedFloat;
             sight = sightFloat;
             stealth = stealthFloat;
-            name = nameString;
+            name = nameInt;
         }
         public void Hunt() {
             Random RNG = new Random();
@@ -53,7 +55,8 @@ namespace Helloworld
             }
             if (food >= 2) {
                 for (int z = 1; z < food; z++) {
-                    Entities.NewHunterList.Add(new Hunter(4, 4, 4, name + "C" + z + "D" + Entities.Day));
+                    Entities.NewHunterList.Add(new Hunter(4, 4, 4, Entities.NamesUsed + 1));
+                    Entities.NamesUsed++;
                 }
             }
         }
@@ -70,12 +73,13 @@ namespace Helloworld
             File.Delete("EvolutionSimulationOutput.txt");
             using StreamWriter outputFile = new StreamWriter("EvolutionSimulationOutput.txt");
 
-            Entities.HunterList.Add(new Hunter(4, 4, 4, "A"));
+            Entities.HunterList.Add(new Hunter(4, 4, 4, 0));
             for (int y = 1; y < 6; y++) {
                 Entities.Day = y;
                 outputFile.WriteLine(string.Concat(Enumerable.Repeat("-", 100)) + "\n" + "Day: " + Entities.Day + "\n" + string.Concat(Enumerable.Repeat("-", 100)));
                 Entities.Rabbits = 10;
                 Entities.NewHunterList.Clear();
+                ShuffleHunterList();
                 foreach (Hunter Target in Entities.HunterList) {
                     Target.Hunt();
                     outputFile.WriteLine("Name: " + Target.name + "\nFood: " + Target.food + "\n");
@@ -84,6 +88,10 @@ namespace Helloworld
                 Entities.HunterList.Clear();
                 Entities.HunterList.AddRange(Entities.NewHunterList);
             }
+        }
+        static void ShuffleHunterList() {
+            Random rnglistorder = new Random();
+            Entities.HunterList.OrderBy(a => rnglistorder.Next());
         }
     }
 }
